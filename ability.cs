7319ec6009283b75b423dda3f81286ed9952fd35
DimensionPlant1 <dimensionplant@gmail.com>
2024-07-18 17:10:48 +0200

    first commit - committing what I tinkered with this morning.

---
+++ ability.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class ability : Control
+{
+
+	private Timer _cooldown;
+	public double CD {get => _cooldown.TimeLeft;}
+	private RichTextLabel _display;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_display = GetNode("Container").GetNode<RichTextLabel>("TimeDisplay");
+		_cooldown = GetNode<Timer>("Brightness");
+		_cooldown.Start();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
