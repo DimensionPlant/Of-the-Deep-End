f22e8f4455a9a7290fbb8f872f0eab89d6f83968
EntropicEquanimity <joshuahunt25@hotmail.com>
2024-07-23 19:17:40 -0600

    Systems work
    1. Inventory (should be working already, haven't extensively tested it yet but adding items should be working.)
    2. Entity / Character data (Storage of data in BaseEntity)
    3. Added item stacking support (for the most part)

--- Objects/Items/BaseItem.cs
+++ Objects/Items/BaseItem.cs
@@ -7,8 +7,11 @@ public abstract class BaseItem : Sprite
 	[Export] public string itemDescription;
 	[Export] public ItemRarity itemRarity;
 	[Export] public int dropWeight = 5;
+	public int ItemCount { get => _itemCount; }
+	protected int _itemCount = 1;
 	
 	public abstract void OnEquip();
+	public abstract void OnIncreaseCount();
 	public abstract void OnTick();
 	public abstract void OnUnequip();
 }
--- Objects/Items/Inventory.cs
+++ Objects/Items/Inventory.cs
@@ -7,13 +7,33 @@ public class Inventory : Node
 {
 	public List<BaseItem> items = new List<BaseItem>();
 	
-	public bool AddItem(){
+	//Triggers when item is first added, usually for like one-off effects. 
+	public bool AddItem(BaseItem item){
+		if(item != null){ //Check if item is null. 
+			foreach(BaseItem _item in items){ //Go through every item in the inventory
+				if(_item.itemName == item.itemName){ //Check if the item is already in the inventory. 
+					_item.OnIncreaseCount();
+					item.QueueFree();
+				}
+				else {
+					item.OnEquip();
+					item.Hide();
+				}
+			}
+			return true;
+		}
 		return false;
 	}
-	public bool RemoveItem(){
+	
+	//Triggers when item is deleted / removed
+	public bool RemoveItem(BaseItem item){
 		return false;
 	}
+	
+	//Ticks once every Physics frame
 	public void TickItems(){
-		//Go through all items that have a ticking effect and call the corresponding function. 
+		foreach(BaseItem item in items){
+			item.OnTick();
+		}
 	}
 }
