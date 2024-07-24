1a95c17195f5a0504fb4055ec8618554ef78f2b0
DimensionPlant1 <dimensionplant@gmail.com>
2024-07-24 18:18:11 +0200

    Small UI changes
    Also the Item Sprite sheet has a single pinky on it

--- Objects/LanternDisplay.cs
+++ Objects/LanternDisplay.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class lantern : TextureRect
+public partial class LanternDisplay : TextureRect
 {
 
 	private Timer _bright;
--- Scenes/AbilityDisplay.cs
+++ Scenes/AbilityDisplay.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class ability : Control
+public partial class AbilityDisplay : Control
 {
 
 	private Timer _cooldown;
